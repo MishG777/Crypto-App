56a73e50c674211c6214bfc2b3bbc7cd0ccd3f53
misho <mishiko8gb@gmail.com>
2022-12-28 21:04:13 +0400

    some_css

--- src/components/Navbar/MainHeader.js
+++ src/components/Navbar/MainHeader.js
@@ -3,36 +3,43 @@ import { NavLink, Link, useNavigate } from "react-router-dom";
 import classes from "./MainHeader.module.css";
 
 const MainHeader = ({ logOutHandler, logIn, gotCurrency }) => {
+  // const [menuOpen, setmenuOpen] = useState(false);
+
   const navigate = useNavigate();
 
   const currencyHandler = (e) => {
     const currency = e.target.value.toLowerCase();
-    // setHeaderCurrency(currency);
     navigate(`/all-coins?currency=${currency}`);
     gotCurrency(currency);
   };
+
+  // const setMenu = () => {
+  //   setmenuOpen(!menuOpen);
+  // };
 
   return (
     <header className={classes.header}>
       <h1 className={classes.logo}>CRYPToAPP</h1>
 
       {logIn && (
-        <ul>
-          <select onChange={currencyHandler} className={classes.currency}>
-            <option>USD</option>
-            <option>EUR</option>
-          </select>
-          <li>
-            <NavLink id={classes.coins} to="/all-coins">
-              Search Coins
-            </NavLink>
-          </li>
-          <li>
-            <Link to="/auth-registration-page" onClick={logOutHandler}>
-              Log Out
-            </Link>
-          </li>
-        </ul>
+        <div className={classes.bar}>
+          <ul>
+            <select onChange={currencyHandler} className={classes.currency}>
+              <option>USD</option>
+              <option>EUR</option>
+            </select>
+            <li>
+              <NavLink id={classes.coins} to="/all-coins">
+                Search Coins
+              </NavLink>
+            </li>
+            <li>
+              <Link to="/auth-registration-page" onClick={logOutHandler}>
+                Log Out
+              </Link>
+            </li>
+          </ul>
+        </div>
       )}
     </header>
   );
